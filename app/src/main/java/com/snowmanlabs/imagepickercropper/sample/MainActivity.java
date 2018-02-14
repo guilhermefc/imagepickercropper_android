76c52c6c0d483c8431b885403d5f2953d52fe131
josesantos <jose.l.santos88@gmail.com>
2018-02-14 13:20:00 -0200

    Aspectio Ratio feature implemented
    create image cropper with fixed aspect ratio feature
    
    create image cropper with fixed aspect ratio feature

--- app/src/main/java/com/snowmanlabs/imagepickercropper/sample/MainActivity.java
+++ app/src/main/java/com/snowmanlabs/imagepickercropper/sample/MainActivity.java
@@ -21,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private static final int IMAGE_REQUEST_CODE = 101;
     private Button picButton;
     private ImageView image;
+    private Button fixedRatioButton;
 
-    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void inflateViews() {
         picButton = findViewById(R.id.picButton);
+        fixedRatioButton = findViewById(R.id.fixedRatioButton);
         image = findViewById(R.id.image);
     }
 
@@ -44,17 +47,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(PickerActivity.getIntent(getApplicationContext(), "Select Source"), IMAGE_REQUEST_CODE);
             }
         });
+
+        fixedRatioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(PickerActivity.getIntent(getApplicationContext(), "Select Source", new PickerActivity.AspectRatioWrapper(16, 8)), IMAGE_REQUEST_CODE);
+            }
+        });
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(data != null && resultCode == Activity.RESULT_OK) {
-            if (IMAGE_REQUEST_CODE == requestCode) {
-                if(data.getExtras().containsKey("image")){
-                    Uri mImageUri = (Uri) data.getExtras().get("image");
-                    image.setImageURI(mImageUri);
-                }
+        if (data != null && data.getExtras() != null && resultCode == Activity.RESULT_OK) {
+            if (IMAGE_REQUEST_CODE == requestCode && data.getExtras().containsKey("image")) {
+                Uri mImageUri = (Uri) data.getExtras().get("image");
+                image.setImageURI(mImageUri);
             }
         }
     }
--- library/src/main/java/com/snowmanlabs/imagepickercropper/PickerActivity.java
+++ library/src/main/java/com/snowmanlabs/imagepickercropper/PickerActivity.java
@@ -10,15 +10,18 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
+import java.io.Serializable;
 
 import id.zelory.compressor.Compressor;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -31,7 +34,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public class PickerActivity extends AppCompatActivity implements SourceChooserDialog.SourceChooser {
 
-    private static final String TITLE = "title";
+    private static final String TITLE = "screenTitle";
     public static final String IMAGE = "image";
     private static final int PERMISSIONS_REQUEST_CAMERA = 101;
     private static final int PERMISSIONS_REQUEST_GALLERY = 102;
@@ -41,10 +44,14 @@ public class PickerActivity extends AppCompatActivity implements SourceChooserDi
     private static final String FILENAME = "image.jpeg";
     private static final String IMAGE_WRAPPER = "image_wrapper";
     private static final String SOURCE_CHOOSER = "source_chooser";
+    private static final String ASPECT_RATIO = "aspect_ratio";
+    private static final String TAG = PickerActivity.class.getSimpleName();
 
     private ImageWrapper mImageWrapper;
     private SourceChooserDialog sourceChooser;
-    private String title;
+    private String screenTitle;
+    private boolean hasAspectRatio;
+    private AspectRatioWrapper aspectRatioWrapper;
 
     public static Intent getIntent(Context context, String title) {
         Intent intent = new Intent(context, PickerActivity.class);
@@ -52,18 +59,32 @@ public class PickerActivity extends AppCompatActivity implements SourceChooserDi
         return intent;
     }
 
+    public static Intent getIntent(Context context, String title, AspectRatioWrapper aspectRatioWrapper) {
+        Intent intent = new Intent(context, PickerActivity.class);
+        intent.putExtra(TITLE, title);
+        intent.putExtra(ASPECT_RATIO, aspectRatioWrapper);
+        return intent;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState != null && savedInstanceState.containsKey(IMAGE_WRAPPER)) mImageWrapper = (ImageWrapper) savedInstanceState.getSerializable(IMAGE_WRAPPER);
-        if (savedInstanceState != null && savedInstanceState.containsKey(SOURCE_CHOOSER)) sourceChooser = (SourceChooserDialog) savedInstanceState.getSerializable(SOURCE_CHOOSER);
+        if (savedInstanceState != null && savedInstanceState.containsKey(IMAGE_WRAPPER))
+            mImageWrapper = (ImageWrapper) savedInstanceState.getSerializable(IMAGE_WRAPPER);
+        if (savedInstanceState != null && savedInstanceState.containsKey(SOURCE_CHOOSER))
+            sourceChooser = (SourceChooserDialog) savedInstanceState.getSerializable(SOURCE_CHOOSER);
 
         Bundle extras = getIntent().getExtras();
-        title = extras.getString(TITLE);
+        screenTitle = extras.getString(TITLE);
+
+        if (getIntent().getExtras() != null && getIntent().getExtras().containsKey(ASPECT_RATIO)) {
+            this.aspectRatioWrapper = (AspectRatioWrapper) getIntent().getExtras().get(ASPECT_RATIO);
+            hasAspectRatio = true;
+        }
 
         if (sourceChooser == null) {
-            sourceChooser = SourceChooserDialog.getInstance(title);
+            sourceChooser = SourceChooserDialog.getInstance(screenTitle);
             sourceChooser.show(getSupportFragmentManager(), "SourceChooser");
             sourceChooser.addEventListener(this);
         }
@@ -77,11 +98,7 @@ public class PickerActivity extends AppCompatActivity implements SourceChooserDi
 
     @Override
     public void onChooseCamera() {
-        if (ContextCompat.checkSelfPermission(PickerActivity.this,
-                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(PickerActivity.this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(PickerActivity.this,
-                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-
+        if (isPermissionsForCameraGranted()) {
             mImageWrapper = new ImageWrapper();
             mImageWrapper.setFileName(FILENAME);
 
@@ -101,7 +118,6 @@ public class PickerActivity extends AppCompatActivity implements SourceChooserDi
             intent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 
-
             intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
             startActivityForResult(intent, REQUEST_TAKE_PICTURE);
@@ -113,22 +129,30 @@ public class PickerActivity extends AppCompatActivity implements SourceChooserDi
 
     }
 
+    private boolean isPermissionsForCameraGranted() {
+        return ContextCompat.checkSelfPermission(PickerActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(PickerActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(PickerActivity.this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
     @Override
     public void onChooseGallery() {
-        if (ContextCompat.checkSelfPermission(PickerActivity.this,
-                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(PickerActivity.this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-
+        if (isPermissionsForGalleryGranted()) {
             Intent intent = new Intent();
             intent.setType("image/*");
             intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent, title), REQUEST_LOAD_IMAGE);
-
-
+            startActivityForResult(Intent.createChooser(intent, screenTitle), REQUEST_LOAD_IMAGE);
         } else {
             ActivityCompat.requestPermissions(PickerActivity.this,
                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_GALLERY);
         }
+    }
+
+    private boolean isPermissionsForGalleryGranted() {
+        return ContextCompat.checkSelfPermission(PickerActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(PickerActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
     }
 
 
@@ -137,90 +161,69 @@ public class PickerActivity extends AppCompatActivity implements SourceChooserDi
         super.onSaveInstanceState(outState);
 
         outState.putSerializable(IMAGE_WRAPPER, mImageWrapper);
-//        outState.putSerializable(SOURCE_CHOOSER, sourceChooser);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        boolean crop = true;
         Uri outputUri = null;
         if (resultCode == RESULT_OK) {
             if (requestCode == REQUEST_TAKE_PICTURE) {
                 outputUri = mImageWrapper.getUri();
-                if (outputUri != null)
-                    if (crop) {
-                        CropImage.activity(outputUri)
-                                .setGuidelines(CropImageView.Guidelines.ON)
-                                .start(this);
-                        return;
-                    }
+                if (outputUri != null) {
+                    startCropImage(outputUri);
+                }
 
             } else if (requestCode == REQUEST_LOAD_IMAGE && null != data) {
                 outputUri = data.getData();
-                if (outputUri != null)
-                    if (crop) {
-                        CropImage.activity(outputUri)
-                                .setGuidelines(CropImageView.Guidelines.ON)
-                                .start(this);
-                        return;
-                    }
+                if (outputUri != null) {
+                    startCropImage(outputUri);
+                }
 
             } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
                 CropImage.ActivityResult result = CropImage.getActivityResult(data);
                 outputUri = result.getUri();
             }
 
-            compressImage(new File(outputUri.getPath()));
-
+            if (outputUri != null) {
+                compressImage(new File(outputUri.getPath()));
+            }else {
+                onUriNull();
+            }
         }
 
+    }
+
+    private void onUriNull() {
+        Intent resultIntent = new Intent();
+        setResult(Activity.RESULT_CANCELED, resultIntent);
+        sourceChooser.dismiss();
+        finish();
+    }
+
+    private void startCropImage(Uri outputUri) {
+        if (hasAspectRatio) {
+            CropImage.activity(outputUri)
+                    .setAutoZoomEnabled(true)
+                    .setAspectRatio(aspectRatioWrapper.getRatioX(), aspectRatioWrapper.getRatioY())
+                    .start(this);
+        } else {
+            CropImage.activity(outputUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this);
+        }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_GALLERY:
-                // If request is cancelled, the  result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    onChooseGallery();
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-                    //TODO: permission denied
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                break;
-            case PERMISSIONS_REQUEST_CAMERA:
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    onChooseCamera();
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-                    //TODO: permission denied
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                break;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_GALLERY && grantResults.length > 0) {
+            onChooseGallery();
+        } else if (requestCode == PERMISSIONS_REQUEST_CAMERA && grantResults.length > 0) {
+            onChooseCamera();
         }
     }
 
-    private void compressImage(File file){
+    private void compressImage(File file) {
 
         new Compressor(this)
                 .setQuality(75)
@@ -244,8 +247,26 @@ public class PickerActivity extends AppCompatActivity implements SourceChooserDi
                 }, new Consumer<Throwable>() {
                     @Override
                     public void accept(Throwable throwable) {
-                        throwable.printStackTrace();
+                        Log.e(TAG, throwable.toString());
                     }
                 });
+    }
+
+    public static class AspectRatioWrapper implements Serializable {
+        private int ratioX;
+        private int ratioY;
+
+        public AspectRatioWrapper(int ratioX, int ratioY) {
+            this.ratioX = ratioX;
+            this.ratioY = ratioY;
+        }
+
+        int getRatioX() {
+            return ratioX;
+        }
+
+        int getRatioY() {
+            return ratioY;
+        }
     }
 }
